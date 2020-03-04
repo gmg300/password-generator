0726d95a03ace7845ee1d1a04ee851045916363b
Garrett <garrettgassensmith@gmail.com>
2020-03-04 15:09:41 -0600

    Fixed character generation issues, new and improved README and versioning, minor refactor of a js function

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# Password Generator v0.2
+# Password Generator v0.3.0
+
+![Password Generator Gif](assets/images/password-generator.gif)
 
 ## Description
-A password generator using a simple UI and javascript to generate passwords.
+A password generator using a simple Bootstrap UI and javascript.
 
 Users can customize the password length as well as what types of characters are included in the password. Settings are validated and the result is written to the page. User settings are created with a number input and checkboxes and invalid inputs return an error message to the user. 
 
-The biggest challenge for me was correctly validating the user settings and returning error messages to the user.
+**v0.3.0**
+* Fixed - Code has been refactored so that every type of character is used when checked by the user no matter the length of the password
+* Improved - Reorganized error messaging as it's own function
 
-In future implementations I'd like to refactor the code for efficiency, and add a button to toggle password visibility and a button to copy the generated password to the user's clipboard. 
+**Future Versions**
+* Create a ranged slider bar to sit along side the number input box
+* Refactor the character generation so that the order of random characters in each loop through is also randomized
+* Un-spaghettify the generatePassword function
 
 ## Usage
 https://gmg300.github.io/password-generator/
@@ -15,10 +22,11 @@ https://gmg300.github.io/password-generator/
 ## Credits
 GMG300 https://github.com/gmg300
 
-Bootstrap v4.4.1 https://getbootstrap.com/
+<img alt="Bootstrap Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg" width="30" height="30"> Bootstrap v4.4.1 https://getbootstrap.com/
 
 These videos helped me develop my psuedo-code before creating my own password generator
 * https://www.youtube.com/watch?v=duNmhKgtcsI
 * https://www.youtube.com/watch?v=iKo9pDKKHnc√
 
 ## Contributing
+GMG300
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,39 +7,31 @@ var includeLowercase = document.querySelector("#includeLowercase");
 var includeNumbers = document.querySelector("#includeNumbers");
 var includeSymbols = document.querySelector("#includeSymbols");
 
-
 // Password character option arrays
 var lettersUpperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lettersLowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var symbolsArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '=', '<', '>', '/', ',', '.'];
 
+// Randomizer functions
 function randomUpper() {
-    var letterUpper = lettersUpperArr[Math.floor(Math.random() * lettersUpperArr.length)]
+    var letterUpper = lettersUpperArr[Math.floor(Math.random() * lettersUpperArr.length)];
     return letterUpper;
 }
 function randomLower() {
-    var letterLower = lettersLowerArr[Math.floor(Math.random() * lettersLowerArr.length)]
+    var letterLower = lettersLowerArr[Math.floor(Math.random() * lettersLowerArr.length)];
     return letterLower;
 }
 function randomNumber() {
-    var number = numbersArr[Math.floor(Math.random() * numbersArr.length)]
+    var number = numbersArr[Math.floor(Math.random() * numbersArr.length)];
     return number;
 }
 function randomSymbol() {
-    var symbol = symbolsArr[Math.floor(Math.random() * symbolsArr.length)]
+    var symbol = symbolsArr[Math.floor(Math.random() * symbolsArr.length)];
     return symbol;
 }
-var characterOptions = {
-  upper: randomUpper(),
-  lower: randomLower(), 
-  numbers: randomNumber(),
-  symbols: randomSymbol()
-};
 
-console.log(characterOptions.upper)
-
-
+// Generator function
 function generatePassword() {
   var password = ''; // Set password and options variables
   var length = parseInt(passwordLength.value); // Get user settings
@@ -48,7 +40,7 @@ function generatePassword() {
   var numbers = includeNumbers.checked;
   var symbols = includeSymbols.checked;
   var settingsCount = upper + lower + numbers + symbols;
-  var settings = [
+  var settings = [ // Create settings as objects to give them a key associated with a true or false value 
     {
       upper
     }, 
@@ -61,49 +53,45 @@ function generatePassword() {
     {
       symbols
     }
-  ].filter(setting => { // filter out unchecked settings
+  ].filter(setting => { // filter out unchecked settings - https://youtu.be/duNmhKgtcsI?t=1380
     return Object.values(setting)[0];
   });
-
   if(length < 8 || length > 128 || isNaN(length)) { // Validate length setting
     return 'Password length must be a number between 8 and 128.';
   }
   if(settingsCount < 1) { // Validate character types setting
     return 'You must include at least one type of character.';
   }
-  
-  // Loop through length generating random character from selected options and append it to password string
-  for(i = 0; i < length; i += settingsCount) {
-    characterOptions = {
+  for(i = 0; i < length; i += settingsCount) { // Loop through each type of random function for set length
+    var characterOptions = {
       upper: randomUpper(),
       lower: randomLower(), 
       numbers: randomNumber(),
       symbols: randomSymbol()
     };
-    settings.forEach(setting => {
+    settings.forEach(setting => { // Loop through each object by key - https://youtu.be/duNmhKgtcsI?t=1568
       var characterType = Object.keys(setting)[0];
-      console.log(characterType)
       password += characterOptions[characterType];
     });
   }
   return password;
 }
 
+function errorMessaging(password) {
+  if(password == 'Password length must be a number between 8 and 128.' || password == 'You must include at least one type of character.' ) {
+    passwordText.style.color = 'red';
+    passwordText.style.fontWeight = 'bold';
+  } else {
+    passwordText.style.color = 'hsl(206, 17%, 28%)';
+    passwordText.style.fontWeight = 'normal';
+  }
+}
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  if(password == 'Password length must be a number between 8 and 128.' || password == 'You must include at least one type of character.' ) {
-    passwordText.style.color = 'red'
-    passwordText.style.fontWeight = 'bold'
-  } else {
-    passwordText.style.color = 'hsl(206, 17%, 28%)'
-    passwordText.style.fontWeight = 'normal'
-  }
+  errorMessaging(password);
   passwordText.value = password;
   
 }
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // Add event listener to generate button
